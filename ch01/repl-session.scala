b9f654fb122267c056a6b0422c582aa8f32fb03e
Sreeramaraju <sreeramaraju@gmail.com>
2013-08-30 20:04:23 -0700

    Added chapter01 exercises

---
+++ ch01/repl-session.scala
@@ -0,0 +1,15 @@
+import scala.math._
+import scala.math.BigInt._
+import scala.util._
+
+// Use BigInt to compute 2power1024
+println("2 power 1024 using BigInt "+BigInt(2).pow(1024))
+
+//Create Random Filename or Director using BigInt and convert it using base 36
+println("Generating radom filename "+BigInt(128,Random).toString(36))
+
+
+// First Character of String Scala
+
+println ("Frist Character of Scala is "+ "Scala"(0))
+println ("Last character of Scala is "+ "Scala".last)
